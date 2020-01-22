e16b520f2a8c2bc4bee89eb982d5f26397b855ec
Adam Matan <adam@matan.name>
2020-01-22 23:31:01 +0200

    Solve day 3 (part 2)

--- 2019/day_3/solve.py
+++ 2019/day_3/solve.py
@@ -1,37 +1,57 @@
 #!/usr/bin/env python3
+'''
+Solves "Day 3: Crossed Wires" from adventofcode.com.
+'''
+
 from operator import add
 
-
-DIRECTIONS = {
-    'D': (0, -1),
-    'U': (0, 1),
-    'L': (-1, 0),
-    'R': (1, 0)
-}
-
 def get_wire_coordinates(wire):
-    '''Returns a set of coordinates covered by the wire'''
+    '''Returns a list of coordinates covered by the wire. A wire is a list of
+    directions and distances originating from (0, 0).
+    For example, ['R8','U5','L5','D3'] should return [ (0,0), (1, 0), (2, 0) ...]'''
+    DIRECTIONS = {
+        'D': (0, -1),
+        'U': (0, 1),
+        'L': (-1, 0),
+        'R': (1, 0)
+    }
     point = (0, 0)
-    coordinates = set([point])
+    coordinates = [point]
     for path in wire:
         direction = path[0]
         length = int(path[1:])
         for _ in range(length):
             point = tuple(map(add, DIRECTIONS[direction], point))
-            coordinates.add(point)
+            coordinates.append(point)
     return(coordinates)
 
 def manhattan(point):
     '''Manhattan distance from a point to (0, 0)'''
     return abs(point[0]) + abs(point[1])
 
-def solve(wire_1, wire_2):
+
+def get_intersection(path_1, path_2):
+    '''Returns a set of interesecting coordinates between two paths'''
+    intersections = set(path_1).intersection(set(path_2) - {(0,0)})
+    return intersections
+
+def solve_step_1(wire_1, wire_2):
+    '''Returns the Manhattan distance of the intersection nearest to (0, 0)'''
     path_1, path_2 = get_wire_coordinates(wire_1), get_wire_coordinates(wire_2)
-    intersections = path_1.intersection(path_2 - {(0,0)})
+    intersections = get_intersection(path_1, path_2)
     rv = min(list(map(manhattan, intersections)))
     return rv
 
+def solve_step_2(wire_1, wire_2):
+    '''Returns the intersection whose combined traverse distance is minimal.
+    The combined traverse distance is the combined wire length from the origin
+    to the intersection.'''
+    path_1, path_2 = get_wire_coordinates(wire_1), get_wire_coordinates(wire_2)
+    intersections = get_intersection(path_1, path_2)
+    distances = [path_1.index(intersection)+path_2.index(intersection) for intersection in intersections]
+    return(min(distances))
+    
 if __name__ == '__main__':
     with open('input.txt') as f:
         WIRES = [line.strip().split(',') for line in f]
-    print(solve(*WIRES))
+    print(solve_step_1(*WIRES))
--- 2019/day_3/test.py
+++ 2019/day_3/test.py
@@ -1,5 +1,5 @@
 import unittest
-from solve import manhattan, solve
+from solve import manhattan, solve_step_1, solve_step_2, get_wire_coordinates
 
 class TestDay3(unittest.TestCase):
 
@@ -9,23 +9,22 @@ class TestDay3(unittest.TestCase):
 
     def test_solve(self):
         wires = ['R8','U5','L5','D3'], ['U7','R6','D4','L4']
-        self.assertEqual( solve(*wires), 6 )
+        self.assertEqual( solve_step_1(*wires), 6 )
         
         wires = ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'], \
                 ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83']
-        self.assertEqual( solve(*wires), 159 )
+        self.assertEqual( solve_step_1(*wires), 159 )
 
         wires = ['R98', 'U47', 'R26', 'D63', 'R33' ,'U87', 'L62', 'D20', 'R33', 'U53', 'R51'], \
                 ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7']
-        
-        self.assertEqual( solve(*wires), 135 )
+        self.assertEqual( solve_step_1(*wires), 135 )
 
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
+        with open('input.txt') as f:
+            wires = [line.strip().split(',') for line in f]
+        
+        self.assertEqual( solve_step_1(*wires), 5319 )
+        self.assertEqual( solve_step_2(*wires), 122514 )
 
 if __name__ == '__main__':
     unittest.main()
+    
