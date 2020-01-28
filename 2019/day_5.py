91c4e4eb76a5eb4d6808cd036cad2bf1f02ee553
Adam Matan <adam@matan.name>
2020-01-28 23:20:17 +0200

    WIP

--- 2019/day_5.py
+++ 2019/day_5.py
@@ -3,7 +3,15 @@
 import json
 from operator import add, mul
 import sys
+import logging
 
+logger = logging.getLogger('root')
+FORMAT = "[%(funcName)30s() ]   %(message)s"
+logging.basicConfig(format=FORMAT)
+logger.setLevel(logging.DEBUG)
+
+
+# logging.basicConfig(level=logging.DEBUG)
 # Read opecode (if := 99 then exit)
 # Add position prefix (s = '0' * (5-len(s)) + s)
 # Parse position modes and set operands
@@ -11,18 +19,22 @@ import sys
 # If read mode - read, ptr += 2
 
 def parse_opcode_and_modes(operator):
-    operator = '0' * (5-len(operator)) + operator
-    opcode = operator[3:]
-    modes = operator[:3]
+    padded_operator = str(operator)
+    padded_operator = '0' * (5-len(padded_operator)) + padded_operator
+    opcode = int(padded_operator[3:])
+    modes = [int(i) for i in padded_operator[:3]]
+    logger.debug(f'Operator: {operator:<5}'
+                 f'Padded operator: {padded_operator:<10}'
+                 f'Opcode: {opcode:<5}'
+                 f'Modes: {str(modes):<10}')
     return opcode, modes
 
 def get_operand(opcodes, ptr, mode):
-    print(f'DEBUG {opcodes} {ptr:<10} {mode:<10}')
     if int(mode) == 0:
         rv = int(opcodes[int(opcodes[ptr])])
     else:
         rv = int(opcodes[ptr])
-    print(f'\t Operand: {rv}, mode: {mode}, ptr: {ptr}')
+    logger.debug(f'Operand: {rv}, mode: {mode}, ptr: {ptr}')
     return rv
 
 def get_operands(opcodes, ptr, modes, count):
@@ -33,21 +45,35 @@ def get_operands(opcodes, ptr, modes, count):
 
 def run(opcodes, stdin=sys.stdin, stdout=sys.stdout):
     ptr = 0
-    while (operator := intcodes[ptr]) != '99' and (ptr < len(intcodes)):
+    while (operator := intcodes[ptr]) != 99 and (ptr < len(intcodes)):
+        logger.debug(f'ptr: {ptr}, opcodes[ptr]: {opcodes[ptr]}')
         opcode, modes = parse_opcode_and_modes(operator)
-        print(opcode, modes)
-        # Zero-pad to length 5
-        if opcode in ('02', '03'):
-            op = mul if opcode == '02' else add
+        if opcode in (1, 2):
+            op = mul if opcode == 2 else add
             operands = get_operands(opcodes, ptr+1, modes, 2)
             destination_ptr = int(opcodes[ptr+3])
-            print(f'Operands: {operands}, destination_ptr: {destination_ptr}')
+            logger.debug(f'<1,2> Opcodes: {str(opcodes[ptr:ptr+3]):<20} Operands: {operands}, destination_ptr: {destination_ptr}')
             opcodes[destination_ptr] = op(*operands)
-            print(opcodes)
-        break
+            ptr += 4
+        elif opcode == 3:
+            operands = get_operands(opcodes, ptr+1, modes, 1)
+            destination_ptr = int(opcodes[ptr+1])
+            logger.debug(f'<3> Opcodes: {str(opcodes[ptr:ptr+2]):<20} Operands: {operands}, destination_ptr: {destination_ptr}')
+            opcodes[operands[0]] = int(stdin.read())
+            logger.debug(f'<3> Read {opcodes[operands[0]]} from input stream')
+            ptr += 2
+        elif opcode == 4:
+            operands = get_operands(opcodes, ptr+1, modes, 1)
+            logger.debug(f'<4> Opcodes: {str(opcodes[ptr:ptr+2]):<20} Operands: {operands}')
+            stdout.write(operands[0])
+            ptr += 2
+        else:
+            break
 
 if __name__ == '__main__':
     with open('day_5.txt') as f:
         intcodes = f.read().split(',')
-        intcodes = ['1002', '4', '3', '4', '33']
+        # intcodes = ['1002', '4', '3', '4', '33']
+        intcodes = [int (i) for i in intcodes]
+        logger.info(f'First codes: {str(intcodes[:10])}')
         run(intcodes)
